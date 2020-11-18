5e5d8aea430831cd042efa3dab564d15c783c9ea
Benjamin Runco <bmr55@pitt.edu>
2020-11-17 22:46:03 -0500

    Update led.py

--- led.py
+++ led.py
@@ -1,1 +1,12 @@
+import RPi.GPIO as GPIO
 
+def setup_gpio(pin, warnings):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(warnings)
+    GPIO.setup(pin, GPIO.OUT)
+
+def turn_on(pin):
+    GPIO.output(pin, GPIO.HIGH)
+
+def turn_off(pin):
+    GPIO.output(pin, GPIO.LOW)
